2944c1abbf52c95a9a45b35fb97c11869cdf0db4
Armageddon <lunyx1337@gmail.com>
2020-06-25 22:43:19 +1000

    refactor dbscan

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,62 +1,65 @@
-use std::{collections::HashSet, hash::Hash};
+use indexmap::IndexSet;
+use rustc_hash::{FxHashSet, FxHasher};
+use std::hash::{BuildHasherDefault, Hash};
 
-pub fn dbscan<P, F>(data: &[P], range_query: F, min_points: usize) -> (Vec<Vec<P>>, HashSet<P>)
+type FxIndexSet<T> = IndexSet<T, BuildHasherDefault<FxHasher>>;
+
+pub fn dbscan<'a, DT, P, F>(data: DT, range_query: F, min_points: usize) -> (Vec<Vec<P>>, FxHashSet<P>)
 where
-	P: Eq + Hash + Clone,
-	F: Fn(&P) -> Vec<P>,
+	DT: IntoIterator<Item = &'a P>,
+	P: Eq + Hash + Clone + 'a,
+	F: Fn(&P) -> FxIndexSet<P>,
 {
 	let mut c = 0;
 	let mut clusters = Vec::<Vec<P>>::new();
-	let mut noise = HashSet::<P>::new();
-	let mut solved = HashSet::<P>::new();
-	data.iter().for_each(|p| {
-		if !solved.contains(&p) {
-			let neighbors = range_query(&p);
-			if neighbors.len() < min_points {
-				noise.insert(p.clone());
-				solved.insert(p.clone());
-			} else {
-				match clusters.get_mut(c) {
-					Some(cluster) => cluster.push(p.clone()),
-					None => clusters.push(vec![p.clone()]),
-				}
-				let mut seed_set = neighbors.into_iter().collect::<HashSet<P>>();
-				seed_set.insert(p.clone());
+	let mut noise = FxHashSet::<P>::default();
+	let mut solved = FxHashSet::<P>::default();
+	data.into_iter().for_each(|p| {
+		if solved.contains(&p) {
+			return;
+		}
+		solved.insert(p.clone());
 
-				while !seed_set.is_empty() {
-					seed_set.clone().into_iter().for_each(|q| {
-						if noise.remove(&q) {
-							clusters.get_mut(c).unwrap().push(q.clone());
-						} else if !solved.contains(&q) {
-							clusters.get_mut(c).unwrap().push(q.clone());
-							solved.insert(q.clone());
-							let neighbors = range_query(&q);
-							if neighbors.len() >= min_points {
-								neighbors.iter().for_each(|n| {
-									seed_set.insert(n.clone());
-								});
-							}
-						}
-						seed_set.remove(&q);
-					});
-				}
-				c += 1;
+		let neighbors = range_query(&p);
+		if neighbors.len() < min_points {
+			noise.insert(p.clone());
+		} else {
+			match clusters.get_mut(c) {
+				Some(cluster) => cluster.push(p.clone()),
+				None => clusters.push(vec![p.clone()]),
 			}
+
+			let mut seed_set = neighbors;
+			while let Some(q) = seed_set.pop() {
+				if noise.remove(&q) {
+					clusters.get_mut(c).unwrap().push(q.clone());
+				} else if !solved.contains(&q) {
+					clusters.get_mut(c).unwrap().push(q.clone());
+					solved.insert(q.clone());
+					let neighbors = range_query(&q);
+					if neighbors.len() >= min_points {
+						seed_set.extend(neighbors);
+					}
+				}
+			}
+			c += 1;
 		}
 	});
 	(clusters, noise)
 }
 
-pub fn range_query<'r, P, D: 'r, F: 'r>(data: &'r [P], distance: F, epsilon: D) -> impl Fn(&P) -> Vec<P> + 'r
+pub fn range_query<'a, DT, P, D, F>(data: DT, distance: F, epsilon: D) -> impl Fn(&P) -> FxIndexSet<P> + 'a
 where
-	P: Clone,
-	D: PartialOrd,
-	F: Fn(&P, &P) -> D,
+	DT: IntoIterator<Item = &'a P> + Clone + 'a,
+	P: Eq + Hash + Clone + 'a,
+	D: PartialOrd + 'a,
+	F: Fn(&P, &P) -> D + 'a,
 {
 	move |q: &P| {
-		data.iter()
+		data.clone()
+			.into_iter()
 			.filter(|p| distance(q, &p) <= epsilon)
 			.cloned()
-			.collect::<Vec<P>>()
+			.collect()
 	}
 }
