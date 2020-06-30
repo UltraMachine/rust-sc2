6f50c5f462734c12a6f38819c8a06578227711f8
Armageddon <lunyx1337@gmail.com>
2020-06-30 10:34:39 +1000

    apply formatting to "generate_enums" script

--- generate_enums.py
+++ generate_enums.py
@@ -25,7 +25,7 @@ def parse_simple(d, data):
 				index += 1
 				tmp = f"{key_to_insert}{index}"
 			key_to_insert = tmp
-		key_to_insert = key_to_insert[0].upper()+key_to_insert[1:]
+		key_to_insert = key_to_insert[0].upper() + key_to_insert[1:]
 		units[key_to_insert] = v["id"]
 
 	return units
@@ -62,7 +62,7 @@ def parse_data(data, version=None):
 		if key[0].isdigit():
 			key = "_" + key
 
-		key = key[0].upper()+key[1:]
+		key = key[0].upper() + key[1:]
 		key = key.replace("ResearchResearch", "Research")
 
 		if key in abilities and v["index"] == 0:
@@ -74,11 +74,11 @@ def parse_data(data, version=None):
 			abilities[key] = v["id"]
 
 	# fixes for wrong ids
-	if version == '4.10':
-		upgrades['EnhancedShockwaves'] = 296
-		abilities['GhostAcademyResearchEnhancedShockwaves'] = 822
+	if version == "4.10":
+		upgrades["EnhancedShockwaves"] = 296
+		abilities["GhostAcademyResearchEnhancedShockwaves"] = 822
 	elif version is None:
-		abilities['TerranBuildRefinery'] = 320
+		abilities["TerranBuildRefinery"] = 320
 
 	return (
 		units,
@@ -91,32 +91,51 @@ def parse_data(data, version=None):
 
 def generate():
 	mod = [[], [], ["mod impls;"]]
-	enums_latest = parse_data(load((Path.home()/'Documents'/'StarCraft II'/'stableid.json').open()))
+	enums_latest = parse_data(
+		load((Path.home() / "Documents" / "StarCraft II" / "stableid.json").open())
+	)
 	enums_4_10 = parse_data(
-		load((Path.home()/'Documents'/'StarCraft II'/'stableid_4.10.json').open()),
-		version='4.10'
+		load(
+			(Path.home() / "Documents" / "StarCraft II" / "stableid_4.10.json").open()
+		),
+		version="4.10",
 	)
 
-	for name, file, enum, enum_linux in zip(ENUM_NAMES, FILE_NAMES, enums_latest, enums_4_10):
+	for name, file, enum, enum_linux in zip(
+		ENUM_NAMES, FILE_NAMES, enums_latest, enums_4_10
+	):
 		if enum == enum_linux:
-			generated = f'{DERIVES}\npub enum {name} {{\n'+''.join(
-				f'\t{k} = {v},\n'
-				for k, v in sorted(enum.items(), key=lambda x: x[1])
-			)+'}\n'
+			generated = (
+				f"{DERIVES}\npub enum {name} {{\n"
+				+ "".join(
+					f"\t{k} = {v},\n"
+					for k, v in sorted(enum.items(), key=lambda x: x[1])
+				)
+				+ "}\n"
+			)
 		else:
-			generated = f'#[cfg(target_os = "windows")]\n{DERIVES}\npub enum {name} {{\n'+''.join(
-				f'\t{k} = {v},\n'
-				for k, v in sorted(enum.items(), key=lambda x: x[1])
-			)+'}\n\n'+f'#[cfg(target_os = "linux")]\n{DERIVES}\npub enum {name} {{\n'+''.join(
-				f'\t{k} = {v},\n'
-				for k, v in sorted(enum_linux.items(), key=lambda x: x[1])
-			)+'}\n'
+			generated = (
+				f'#[cfg(target_os = "windows")]\n{DERIVES}\npub enum {name} {{\n'
+				+ "".join(
+					f"\t{k} = {v},\n"
+					for k, v in sorted(enum.items(), key=lambda x: x[1])
+				)
+				+ "}\n\n"
+				+ f'#[cfg(target_os = "linux")]\n{DERIVES}\npub enum {name} {{\n'
+				+ "".join(
+					f"\t{k} = {v},\n"
+					for k, v in sorted(enum_linux.items(), key=lambda x: x[1])
+				)
+				+ "}\n"
+			)
 
-		(Path.cwd()/'src'/'ids'/f'{file}.rs').write_text(generated)
-		mod[0].append(f'mod {file};')
-		mod[1].append(f'pub use {file}::{name};')
-	(Path.cwd()/'src'/'ids'/'mod.rs').write_text('\n\n'.join('\n'.join(part) for part in mod)+'\n')
+		(Path.cwd() / "src" / "ids" / f"{file}.rs").write_text(generated)
+		mod[0].append(f"mod {file};")
+		mod[1].append(f"pub use {file}::{name};")
+	(Path.cwd() / "src" / "ids" / "mod.rs").write_text(
+		"\n\n".join("\n".join(part) for part in mod) + "\n"
+	)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	generate()
