5b194a001842cc0d053dee0665ee55b6a8a594b8
Armageddon <lunyx1337@gmail.com>
2020-07-05 23:26:17 +1000

    example of how to use events system

---
+++ examples/events.rs
@@ -0,0 +1,40 @@
+use rust_sc2::prelude::*;
+
+// Example of how to use events
+
+#[bot]
+#[derive(Default)]
+struct EmptyBot;
+impl Player for EmptyBot {
+	fn get_player_settings(&self) -> PlayerSettings {
+		PlayerSettings::new(Race::Random, None)
+	}
+
+	// Use it like here
+	fn on_event(&mut self, event: Enent) -> SC2Result<()> {
+		match event {
+			Event::UnitDestroyed(tag) => { /* your code here */ }
+			Event::UnitCreated(tag) => {
+				if let Some(u) = self.units.my.units.find_tag(tag) { /* your code here */ }
+			}
+			Event::ConstructionStarted(tag) => {
+				if let Some(u) = self.units.my.structures.find_tag(tag) { /* your code here */ }
+			}
+			Event::ConstructionComplete(tag) => {
+				if let Some(u) = self.units.my.structures.find_tag(tag) { /* your code here */ }
+			}
+		}
+		Ok(())
+	}
+}
+
+fn main() -> SC2Result<()> {
+	let mut bot = EmptyBot::default();
+
+	run_vs_computer(
+		&mut bot,
+		Computer::new(Race::Random, Difficulty::VeryEasy, None),
+		"EverDreamLE",
+		Default::default(),
+	)
+}
