552e2aa5bf6c3535b9490dc3ad10f0fe40ba9fd3
Armageddon <lunyx1337@gmail.com>
2020-08-11 13:39:42 +0300

    add docs for "ramp" module, change visiblity of some items

--- src/ramp.rs
+++ src/ramp.rs
@@ -1,3 +1,6 @@
+//! Data structures for storing data of ramps on the map
+//! with methods for extracting useful info from them.
+
 use crate::{bot::Rs, distance::*, geometry::Point2, pixel_map::ByteMap};
 use std::{
 	cmp::{Ordering, Reverse},
@@ -5,35 +8,37 @@ use std::{
 	fmt,
 };
 
+/// Structured collection of ramps.
 #[derive(Default)]
 pub struct Ramps {
+	/// All ramps on the map.
 	pub all: Vec<Ramp>,
+	/// Ramp to your main base.
 	pub my: Ramp,
+	/// Ramp to opponent's main base.
 	pub enemy: Ramp,
 }
 
 type Pos = (usize, usize);
 
+/// Ramp data structure with some helpful methods.
+/// All ramps stored in [`Ramps`] in [`ramps`](crate::bot::Bot::ramps) field of bot.
 #[derive(Default, Clone)]
 pub struct Ramp {
+	/// All points which belong to this ramp.
 	pub points: Vec<Pos>,
 	height: Rs<ByteMap>,
 	start_location: Point2,
 }
 impl Ramp {
-	pub fn new(points: Vec<Pos>, height: &Rs<ByteMap>, start_location: Point2) -> Self {
+	pub(crate) fn new(points: Vec<Pos>, height: &Rs<ByteMap>, start_location: Point2) -> Self {
 		Self {
 			points,
 			height: Rs::clone(&height),
 			start_location,
-			/*points: points
-			.into_iter()
-			.map(|(x, y)| Point2::new(x as f32, y as f32))
-			.collect(),*/
-			// cache: Default::default(),
-			// cache2: Default::default(),
 		}
 	}
+	/// Returns only upper points of the ramp.
 	pub fn upper(&self) -> Vec<Pos> {
 		let mut max = u8::MIN;
 		let mut result = Vec::new();
@@ -52,6 +57,7 @@ impl Ramp {
 
 		result
 	}
+	/// Returns only lower points of the ramp.
 	pub fn lower(&self) -> Vec<Pos> {
 		let mut min = u8::MAX;
 		let mut result = Vec::new();
@@ -70,7 +76,8 @@ impl Ramp {
 
 		result
 	}
-	pub(crate) fn top_center(&self) -> Option<Pos> {
+	/// Returns center of upper points of the ramp.
+	pub fn top_center(&self) -> Option<Pos> {
 		let ps = self.upper();
 		if ps.is_empty() {
 			None
@@ -80,7 +87,8 @@ impl Ramp {
 			Some((x / ps.len(), y / ps.len()))
 		}
 	}
-	fn bottom_center(&self) -> Option<Pos> {
+	/// Returns center of lower points of the ramp.
+	pub fn bottom_center(&self) -> Option<Pos> {
 		let ps = self.lower();
 		if ps.is_empty() {
 			None
@@ -107,6 +115,21 @@ impl Ramp {
 			Ordering::Less => None,
 		}
 	}
+	/// Returns correct positions to build corner supplies in terran wall.
+	pub fn corner_depots(&self) -> Option<[Point2; 2]> {
+		if let Some(ps) = self.upper2_for_ramp_wall() {
+			let (x, y) = ps[0];
+			let p1 = Point2::new(x as f32 + 0.5, y as f32 + 0.5);
+			let (x, y) = ps[1];
+			let p2 = Point2::new(x as f32 + 0.5, y as f32 + 0.5);
+
+			let center = (p1 + p2) / 2.0;
+
+			return Some(center.circle_intersection(self.depot_in_middle()?, 2.236_068)?); // 5_f32.sqrt()
+		}
+		None
+	}
+	/// Returns correct position to build barrack in terran wall without addon.
 	pub fn barracks_in_middle(&self) -> Option<Point2> {
 		let upper_len = self.upper().len();
 		if upper_len != 2 && upper_len != 5 {
@@ -118,7 +141,7 @@ impl Ramp {
 			let (x, y) = ps[1];
 			let p2 = Point2::new(x as f32 + 0.5, y as f32 + 0.5);
 
-			let intersects = p1.circle_intersection(p2, 2.236_068); // 5_f32.sqrt()
+			let intersects = p1.circle_intersection(p2, 2.236_068)?; // 5_f32.sqrt()
 			let (x, y) = *self.lower().first()?;
 			let lower = Point2::new(x as f32, y as f32);
 
@@ -126,38 +149,7 @@ impl Ramp {
 		}
 		None
 	}
-	pub fn depot_in_middle(&self) -> Option<Point2> {
-		let upper_len = self.upper().len();
-		if upper_len != 2 && upper_len != 5 {
-			return None;
-		}
-		if let Some(ps) = self.upper2_for_ramp_wall() {
-			let (x, y) = ps[0];
-			let p1 = Point2::new(x as f32 + 0.5, y as f32 + 0.5);
-			let (x, y) = ps[1];
-			let p2 = Point2::new(x as f32 + 0.5, y as f32 + 0.5);
-
-			let intersects = p1.circle_intersection(p2, 1.581_138_8); // 2.5_f32.sqrt()
-			let (x, y) = *self.lower().first()?;
-			let lower = Point2::new(x as f32, y as f32);
-
-			return intersects.iter().furthest(lower).copied();
-		}
-		None
-	}
-	pub fn corner_depots(&self) -> Option<[Point2; 2]> {
-		if let Some(ps) = self.upper2_for_ramp_wall() {
-			let (x, y) = ps[0];
-			let p1 = Point2::new(x as f32 + 0.5, y as f32 + 0.5);
-			let (x, y) = ps[1];
-			let p2 = Point2::new(x as f32 + 0.5, y as f32 + 0.5);
-
-			let center = (p1 + p2) / 2.0;
-
-			return Some(center.circle_intersection(self.depot_in_middle()?, 2.236_068)); // 5_f32.sqrt()
-		}
-		None
-	}
+	/// Returns correct position to build barrack in terran wall with addon.
 	pub fn barracks_correct_placement(&self) -> Option<Point2> {
 		self.barracks_in_middle().map(|pos| {
 			if self
@@ -170,10 +162,32 @@ impl Ramp {
 			}
 		})
 	}
+	/// Returns correct position to build supply in middle of wall from 3 supplies.
+	pub fn depot_in_middle(&self) -> Option<Point2> {
+		let upper_len = self.upper().len();
+		if upper_len != 2 && upper_len != 5 {
+			return None;
+		}
+		if let Some(ps) = self.upper2_for_ramp_wall() {
+			let (x, y) = ps[0];
+			let p1 = Point2::new(x as f32 + 0.5, y as f32 + 0.5);
+			let (x, y) = ps[1];
+			let p2 = Point2::new(x as f32 + 0.5, y as f32 + 0.5);
+
+			let intersects = p1.circle_intersection(p2, 1.581_138_8)?; // 2.5_f32.sqrt()
+			let (x, y) = *self.lower().first()?;
+			let lower = Point2::new(x as f32, y as f32);
+
+			return intersects.iter().furthest(lower).copied();
+		}
+		None
+	}
+	/// Returns correct position to build pylon in protoss wall.
 	pub fn protoss_wall_pylon(&self) -> Option<Point2> {
 		let middle = self.depot_in_middle()?;
 		Some(middle + (self.barracks_in_middle()? - middle) * 6.0)
 	}
+	/// Returns correct positions of 3x3 buildings in protoss wall.
 	pub fn protoss_wall_buildings(&self) -> Option<[Point2; 2]> {
 		let middle = self.depot_in_middle()?;
 		let direction = self.barracks_in_middle()? - middle;
@@ -189,6 +203,7 @@ impl Ramp {
 		let wall1 = depots[1] + direction;
 		Some([wall1, middle + direction + (middle - wall1) / 1.5])
 	}
+	/// Returns correct position of unit to close protoss wall.
 	pub fn protoss_wall_warpin(&self) -> Option<Point2> {
 		let middle = self.depot_in_middle()?;
 		let direction = self.barracks_in_middle()? - middle;
