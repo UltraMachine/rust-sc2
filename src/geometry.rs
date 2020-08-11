83d8b845ab0e0b3b36352b0673be57f5fc78311d
Armageddon <lunyx1337@gmail.com>
2020-08-11 13:38:30 +0300

    add docs for "geometry" module

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,3 +1,7 @@
+//! Things you liked (hated) at school, now in SC2.
+//!
+//! Countains various geometric primitives with useful helper methods.
+
 use crate::{distance::Distance, FromProto, IntoProto};
 use sc2_proto::common::{Point, Point2D};
 use std::{
@@ -6,17 +10,22 @@ use std::{
 	ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign},
 };
 
+/// Size of 2D rectangle.
+#[allow(missing_docs)]
 #[derive(Debug, Default, Copy, Clone)]
 pub struct Size {
 	pub x: usize,
 	pub y: usize,
 }
 impl Size {
+	/// Constructs new `Size` structure with given `x` and `y` size.
 	pub fn new(x: usize, y: usize) -> Self {
 		Self { x, y }
 	}
 }
 
+/// Rectangle from (x0, y0) to (x1, y1).
+#[allow(missing_docs)]
 #[derive(Debug, Default, Copy, Clone)]
 pub struct Rect {
 	pub x0: usize,
@@ -25,11 +34,14 @@ pub struct Rect {
 	pub y1: usize,
 }
 impl Rect {
+	/// Constructs new rectangle with given coordinates.
 	pub fn new(x0: usize, y0: usize, x1: usize, y1: usize) -> Self {
 		Self { x0, y0, x1, y1 }
 	}
 }
 
+/// Point on 2D grid, the most frequently used geometric primitive.
+#[allow(missing_docs)]
 #[derive(Debug, Default, Copy, Clone)]
 pub struct Point2 {
 	pub x: f32,
@@ -38,43 +50,59 @@ pub struct Point2 {
 
 #[allow(clippy::len_without_is_empty)]
 impl Point2 {
+	/// Constructs new 2D Point with given coordinates.
 	pub fn new(x: f32, y: f32) -> Self {
 		Self { x, y }
 	}
+	/// Returns new point with offset towards `other` on given distance.
 	pub fn towards(self, other: Self, offset: f32) -> Self {
 		self + (other - self) / self.distance(other) * offset
 	}
+	/// Returns new point with offset towards given angle on given distance.
 	pub fn towards_angle(self, angle: f32, offset: f32) -> Self {
 		self.offset(offset * angle.cos(), offset * angle.sin())
 	}
+	/// Returns new point with given offset.
 	pub fn offset(self, x: f32, y: f32) -> Self {
 		Self {
 			x: self.x + x,
 			y: self.y + y,
 		}
 	}
-	pub fn circle_intersection(self, other: Self, radius: f32) -> [Self; 2] {
-		let vec_to_center = (other - self) / 2.0;
+	/// Returns points where circles with centers `self` and `other`,
+	/// and given radius intersect, or `None` if they aren't intersect.
+	pub fn circle_intersection(self, other: Self, radius: f32) -> Option<[Self; 2]> {
+		if self == other {
+			return None;
+		}
 
+		let vec_to_center = (other - self) / 2.0;
 		let half_distance = vec_to_center.len();
+
+		if radius < half_distance {
+			return None;
+		}
+
 		let remaining_distance = (radius * radius - half_distance * half_distance).sqrt();
 		let stretch_factor = remaining_distance / half_distance;
 
 		let center = self + vec_to_center;
 		let vec_stretched = vec_to_center * stretch_factor;
-		[
+		Some([
 			center + vec_stretched.rotate90(true),
 			center + vec_stretched.rotate90(false),
-		]
+		])
 	}
 
-	// Vector operations
+	/// Returns length of the vector.
 	pub fn len(self) -> f32 {
 		(self.x.powi(2) + self.y.powi(2)).sqrt()
 	}
+	/// Normalizes the vector.
 	pub fn normalize(self) -> Self {
 		self / self.len()
 	}
+	/// Rotates the vector on given angle.
 	pub fn rotate(self, angle: f32) -> Self {
 		let (s, c) = angle.sin_cos();
 		let (x, y) = (self.x, self.y);
@@ -83,40 +111,44 @@ impl Point2 {
 			y: s * x + c * y,
 		}
 	}
+	/// Fast rotation of the vector on 90 degrees.
 	pub fn rotate90(self, clockwise: bool) -> Self {
-		let (x, y) = if clockwise {
-			(self.y, -self.x)
+		if clockwise {
+			Self::new(self.y, -self.x)
 		} else {
-			(-self.y, self.x)
-		};
-		Self { x, y }
+			Self::new(-self.y, self.x)
+		}
 	}
 
-	// Other
+	/// Returns rounded point.
 	pub fn round(self) -> Self {
 		Self {
 			x: (self.x + 0.5) as i32 as f32,
 			y: (self.y + 0.5) as i32 as f32,
 		}
 	}
+	/// Returns point rounded to closest lower integer.
 	pub fn floor(self) -> Self {
 		Self {
 			x: self.x as i32 as f32,
 			y: self.y as i32 as f32,
 		}
 	}
+	/// Returns point rounded to closest greater integer.
 	pub fn ceil(self) -> Self {
 		Self {
 			x: (self.x + 1.0) as i32 as f32,
 			y: (self.y + 1.0) as i32 as f32,
 		}
 	}
+	/// Returns point with absolute coordinates.
 	pub fn abs(self) -> Self {
 		Self {
 			x: self.x.abs(),
 			y: self.y.abs(),
 		}
 	}
+	/// Returns 4 closest neighbors of point.
 	pub fn neighbors4(self) -> [Self; 4] {
 		[
 			self.offset(1.0, 0.0),
@@ -125,6 +157,7 @@ impl Point2 {
 			self.offset(0.0, -1.0),
 		]
 	}
+	/// Returns 4 closest diagonal neighbors of point.
 	pub fn neighbors4diagonal(self) -> [Self; 4] {
 		[
 			self.offset(1.0, 1.0),
@@ -133,6 +166,7 @@ impl Point2 {
 			self.offset(-1.0, 1.0),
 		]
 	}
+	/// Returns 8 closest neighbors of point.
 	pub fn neighbors8(self) -> [Self; 8] {
 		[
 			self.offset(1.0, 0.0),
@@ -145,9 +179,11 @@ impl Point2 {
 			self.offset(-1.0, 1.0),
 		]
 	}
+	/// Returns tuple with point's coordinates.
 	pub fn as_tuple(self) -> (f32, f32) {
 		(self.x, self.y)
 	}
+	/// Converts 2D Point to 3D Point using given `z` value.
 	pub fn to3(self, z: f32) -> Point3 {
 		Point3 {
 			x: self.x,
@@ -156,9 +192,10 @@ impl Point2 {
 		}
 	}
 }
+
 impl PartialEq for Point2 {
 	fn eq(&self, other: &Self) -> bool {
-		(self.x - other.x).abs() < std::f32::EPSILON && (self.y - other.y).abs() < std::f32::EPSILON
+		(self.x - other.x).abs() < f32::EPSILON && (self.y - other.y).abs() < f32::EPSILON
 	}
 }
 impl Eq for Point2 {}
@@ -168,6 +205,7 @@ impl Hash for Point2 {
 		((self.y + 0.5) as i32).hash(state);
 	}
 }
+
 impl From<&Point2> for Point2 {
 	#[inline]
 	fn from(p: &Point2) -> Self {
@@ -180,6 +218,20 @@ impl From<Point2> for (usize, usize) {
 		((p.x + 0.5) as usize, (p.y + 0.5) as usize)
 	}
 }
+
+impl From<(f32, f32)> for Point2 {
+	#[inline]
+	fn from((x, y): (f32, f32)) -> Self {
+		Self { x, y }
+	}
+}
+impl From<Point2> for (f32, f32) {
+	#[inline]
+	fn from(p: Point2) -> Self {
+		p.as_tuple()
+	}
+}
+
 impl Add for Point2 {
 	type Output = Self;
 
@@ -349,6 +401,8 @@ impl IntoProto<Point2D> for Point2 {
 	}
 }
 
+/// Point in 3D game world.
+#[allow(missing_docs)]
 #[derive(Debug, Default, Copy, Clone)]
 pub struct Point3 {
 	pub x: f32,
@@ -356,9 +410,11 @@ pub struct Point3 {
 	pub z: f32,
 }
 impl Point3 {
+	/// Constructs new 3D Point with given coordinates.
 	pub fn new(x: f32, y: f32, z: f32) -> Self {
 		Self { x, y, z }
 	}
+	/// Returns new point with given offset.
 	pub fn offset(self, x: f32, y: f32, z: f32) -> Self {
 		Self {
 			x: self.x + x,
@@ -366,6 +422,7 @@ impl Point3 {
 			z: self.z + z,
 		}
 	}
+	/// Returns rounded point.
 	pub fn round(self) -> Self {
 		Self {
 			x: (self.x + 0.5) as i32 as f32,
@@ -373,19 +430,36 @@ impl Point3 {
 			z: (self.z + 0.5) as i32 as f32,
 		}
 	}
+	/// Returns tuple with point's coordinates.
 	pub fn as_tuple(self) -> (f32, f32, f32) {
 		(self.x, self.y, self.z)
 	}
+	/// Converts 3D Point to 2D Point.
 	pub fn to2(self) -> Point2 {
 		Point2 { x: self.x, y: self.y }
 	}
 }
+
 impl From<Point3> for Point2 {
 	#[inline]
 	fn from(p3: Point3) -> Self {
 		p3.to2()
 	}
 }
+
+impl From<(f32, f32, f32)> for Point3 {
+	#[inline]
+	fn from((x, y, z): (f32, f32, f32)) -> Self {
+		Self { x, y, z }
+	}
+}
+impl From<Point3> for (f32, f32, f32) {
+	#[inline]
+	fn from(p3: Point3) -> Self {
+		p3.as_tuple()
+	}
+}
+
 impl Add for Point3 {
 	type Output = Self;
 
