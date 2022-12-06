7f8b53cdfc02a46b97145a3eda483227518a7f48
Armageddon <lunyx1337@gmail.com>
2022-12-06 18:34:31 +1000

    update examples to use new parser

--- examples/ex_main/mod.rs
+++ examples/ex_main/mod.rs
@@ -1,148 +1,131 @@
-use clap::*;
-use rand::prelude::*;
+use clap::{Parser, Subcommand};
 use rust_sc2::bot::Bot;
 use rust_sc2::prelude::*;
 use std::ops::{Deref, DerefMut};
 
+#[derive(Parser)]
+#[clap(version, author)]
+struct Args {
+	#[clap(long = "LadderServer")]
+	ladder_server: Option<String>,
+	#[clap(long = "OpponentId")]
+	opponent_id: Option<String>,
+	#[clap(long = "GamePort")]
+	host_port: Option<i32>,
+	#[clap(long = "StartPort")]
+	player_port: Option<i32>,
+	#[clap(long = "RealTime")]
+	realtime: bool,
+
+	/// Set game step for bot
+	#[clap(short = 's', long = "step", default_value = "2")]
+	game_step: u32,
+
+	#[clap(subcommand)]
+	command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+	/// Run local game vs Computer
+	Local {
+		/// Set path to map, relative to "Starcraft II/Maps" directory.
+		/// See `https://github.com/UltraMachine/rust-sc2/blob/master/src/paths.rs#L38-L67`
+		#[clap(short, long)]
+		map: String,
+		/// Set opponent race
+		#[clap(short, long, default_value = "Random")]
+		race: Race,
+		/// Set opponent diffuculty
+		#[clap(short, long)]
+		difficulty: Option<Difficulty>,
+		/// Set opponent build
+		#[clap(short = 'b', long)]
+		ai_build: Option<AIBuild>,
+		/// Set sc2 version
+		#[clap(long)]
+		sc2_version: Option<String>,
+		/// Set path to save replay
+		#[clap(long)]
+		save_replay: Option<String>,
+		/// Enable realtime mode
+		#[clap(long)]
+		realtime: bool,
+	},
+	/// Run game Human vs Bot
+	Human {
+		/// Set path to map, relative to "Starcraft II/Maps" directory.
+		/// See `https://github.com/UltraMachine/rust-sc2/blob/master/src/paths.rs#L38-L67`
+		#[clap(short, long)]
+		map: String,
+		/// Set human race
+		#[clap(short, long, default_value = "Random")]
+		race: Race,
+		/// Set human name
+		#[clap(short, long)]
+		name: Option<String>,
+		/// Set sc2 version
+		#[clap(long)]
+		sc2_version: Option<String>,
+		/// Set path to save replay
+		#[clap(long)]
+		save_replay: Option<String>,
+	},
+}
+
 pub(crate) fn main(mut bot: impl Player + DerefMut<Target = Bot> + Deref<Target = Bot>) -> SC2Result<()> {
-	let app = clap_app!(RustyReapers =>
-		(version: crate_version!())
-		(author: crate_authors!())
-		(@arg ladder_server: --LadderServer +takes_value)
-		(@arg opponent_id: --OpponentId +takes_value)
-		(@arg host_port: --GamePort +takes_value)
-		(@arg player_port: --StartPort +takes_value)
-		(@arg game_step: -s --step
-			+takes_value
-			default_value("2")
-			"Sets game step for bot"
-		)
-		(@subcommand local =>
-			(about: "Runs local game vs Computer")
-			(@arg map: -m --map
-				+takes_value
-				"Sets map from the \"Starcraft II/Maps\" folder (no subfolders)"
-			)
-			(@arg race: -r --race
-				+takes_value
-				"Sets opponent race"
-			)
-			(@arg difficulty: -d --difficulty
-				+takes_value
-				"Sets opponent diffuculty"
-			)
-			(@arg ai_build: --("ai-build")
-				+takes_value
-				"Sets opponent build"
-			)
-			(@arg sc2_version: --("sc2-version")
-				+takes_value
-				"Sets sc2 version"
-			)
-			(@arg save_replay: --("save-replay")
-				+takes_value
-				"Sets path to save replay"
-			)
-			(@arg realtime: --realtime "Enables realtime mode")
-		)
-		(@subcommand human =>
-			(about: "Runs game Human vs Bot")
-			(@arg map: -m --map
-				+takes_value
-				"Sets map from the \"Starcraft II/Maps\" folder (no subfolders)"
-			)
-			(@arg race: -r --race *
-				+takes_value
-				"Sets human race"
-			)
-			(@arg name: --name
-				+takes_value
-				"Sets human name"
-			)
-			(@arg sc2_version: --("sc2-version")
-				+takes_value
-				"Sets sc2 version"
-			)
-			(@arg save_replay: --("save-replay")
-				+takes_value
-				"Sets path to save replay"
-			)
-		)
-	)
-	.get_matches();
+	let args = Args::parse();
 
-	let game_step = match app.value_of("game_step") {
-		Some("0") => panic!("game_step must be X >= 1"),
-		Some(step) => step.parse::<u32>().expect("Can't parse game_step"),
-		None => unreachable!(),
-	};
+	if args.game_step == 0 {
+		panic!("game_step must be X >= 1")
+	}
+	bot.set_game_step(args.game_step);
 
-	bot.set_game_step(game_step);
-
-	const LADDER_MAPS: &[&str] = &[
-		"DeathauraLE",
-		"EternalEmpireLE",
-		"EverDreamLE",
-		"GoldenWallLE",
-		"IceandChromeLE",
-		"PillarsofGoldLE",
-		"SubmarineLE",
-	];
-	let mut rng = thread_rng();
-
-	match app.subcommand() {
-		Some(("local", sub)) => run_vs_computer(
+	match args.command {
+		Some(Command::Local {
+			map,
+			race,
+			difficulty,
+			ai_build,
+			sc2_version,
+			save_replay,
+			realtime,
+		}) => run_vs_computer(
 			&mut bot,
-			Computer::new(
-				sub.value_of("race").map_or(Race::Random, |race| {
-					race.parse().expect("Can't parse computer race")
-				}),
-				sub.value_of("difficulty")
-					.map_or(Difficulty::VeryEasy, |difficulty| {
-						difficulty.parse().expect("Can't parse computer difficulty")
-					}),
-				sub.value_of("ai_build")
-					.map(|ai_build| ai_build.parse().expect("Can't parse computer build")),
-			),
-			sub.value_of("map")
-				.unwrap_or_else(|| LADDER_MAPS.choose(&mut rng).unwrap()),
+			Computer::new(race, difficulty.unwrap_or(Difficulty::VeryEasy), ai_build),
+			&map,
 			LaunchOptions {
-				sc2_version: sub.value_of("sc2_version"),
-				realtime: sub.is_present("realtime"),
-				save_replay_as: sub.value_of("save_replay"),
+				sc2_version: sc2_version.as_deref(),
+				realtime,
+				save_replay_as: save_replay.as_deref(),
 			},
 		),
-		Some(("human", sub)) => run_vs_human(
+		Some(Command::Human {
+			map,
+			race,
+			name,
+			sc2_version,
+			save_replay,
+		}) => run_vs_human(
 			&mut bot,
 			PlayerSettings {
-				race: sub
-					.value_of("race")
-					.unwrap()
-					.parse()
-					.expect("Can't parse human race"),
-				name: sub.value_of("name"),
+				race,
+				name: name.as_deref(),
 				..Default::default()
 			},
-			sub.value_of("map")
-				.unwrap_or_else(|| LADDER_MAPS.choose(&mut rng).unwrap()),
+			&map,
 			LaunchOptions {
-				sc2_version: sub.value_of("sc2_version"),
+				sc2_version: sc2_version.as_deref(),
 				realtime: true,
-				save_replay_as: sub.value_of("save_replay"),
+				save_replay_as: save_replay.as_deref(),
 			},
 		),
-		_ => run_ladder_game(
+		None => run_ladder_game(
 			&mut bot,
-			app.value_of("ladder_server").unwrap_or("127.0.0.1"),
-			app.value_of("host_port")
-				.expect("GamePort must be specified")
-				.parse()
-				.expect("Can't parse GamePort"),
-			app.value_of("player_port")
-				.expect("StartPort must be specified")
-				.parse()
-				.expect("Can't parse StartPort"),
-			app.value_of("opponent_id"),
+			args.ladder_server.as_deref().unwrap_or("127.0.0.1"),
+			args.host_port.expect("GamePort must be specified"),
+			args.player_port.expect("StartPort must be specified"),
+			args.opponent_id.as_deref(),
 		),
 	}
 }
